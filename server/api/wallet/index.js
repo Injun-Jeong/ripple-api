cd8070961e5a2b8da4391637ed790383b9a4fa63
injun <dhak45@gmail.com>
2021-08-13 08:26:16 +0000

    feature: Adding the function of transfer

--- server/api/wallet/index.js
+++ server/api/wallet/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const cltr = require('./wallet.ctrl')
 const router = express.Router();
 
-// 계좌 정보 조회
+/* 계좌 정보 조회 */
 router.get('/', cltr.index);
+
+/* 계좌 송금 */
+router.get('/transfer', cltr.transfer);
 
 module.exports = router;
